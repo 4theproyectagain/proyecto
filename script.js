ccd845b32a5ec666ddcc3f2ab9331456f4e526a5
4theproyectagain <camille.tavara.21@hotmail.com>
2026-07-09 17:49:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 const boton = document.getElementById("startButton");
 const video = document.getElementById("video");
+const emotionText = document.getElementById("emotion");
 
 boton.addEventListener("click", async () => {
     try {
+
+        await faceapi.nets.tinyFaceDetector.loadFromUri("./models");
+        await faceapi.nets.faceExpressionNet.loadFromUri("./models");
+
         const stream = await navigator.mediaDevices.getUserMedia({
             video: true
         });
 
         video.srcObject = stream;
 
+        emotionText.innerHTML = "🧠 IA activada, analizando...";
+
+        video.addEventListener("play", () => {
+
+            setInterval(async () => {
+
+                const detections = await faceapi
+                    .detectSingleFace(
+                        video,
+                        new faceapi.TinyFaceDetectorOptions()
+                    )
+                    .withFaceExpressions();
+
+                if (detections) {
+
+                    const expressions = detections.expressions;
+
+                    const emotion = Object.keys(expressions)
+                        .reduce((a, b) =>
+                            expressions[a] > expressions[b] ? a : b
+                        );
+
+                    emotionText.innerHTML =
+                        "😊 " + emotion;
+
+                }
+
+            }, 1000);
+
+        });
+
     } catch (error) {
-        alert("No se pudo acceder a la cámara.");
-        console.error(error);
+    alert(error.message);
+    console.error(error);
+}
     }
-})
+});
